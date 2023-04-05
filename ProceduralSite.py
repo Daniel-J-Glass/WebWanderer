9cbf0d01c1f29630d208f3711eab38895a571ee0
Daniel-J-Glass <danieljglass1@gmail.com>
2023-04-05 00:19:54 -0500

    Added loading screen, improved error handling

--- ProceduralSite.py
+++ ProceduralSite.py
@@ -13,6 +13,8 @@ import openai
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
+from urllib.parse import quote
+
 from PIL import Image
 
 from flask import Flask,url_for,current_app
@@ -45,8 +47,18 @@ class ProceduralSite:
     TODO: Check for parent UUID in database, redirect to current if not.
     TODO: 
     '''
+    caching_string = '''// Cache the current page in the browser's history
+  function cacheCurrentPageInHistory() {
+    var currentUrl = window.location.href;
+    window.history.replaceState(null, null, currentUrl);
+  }
+
+  // Call the function on page load
+  cacheCurrentPageInHistory();'''
+    
     num_retries = 3
-    def __init__(self, parent_uuid="", name = "", visuals="", layout="", purpose = "", context_length = 32, image_steps = 15):
+    model_name = "gpt-3.5-turbo"
+    def __init__(self, parent_uuid="", name = "", visuals="", layout="", purpose = "",context_length = 32, image_steps = 15):
         # Database storage procedure
         
         self.name = name
@@ -67,56 +79,62 @@ class ProceduralSite:
         #post self to firebase, get uuid return
         #self.uuid = 
         self.uuid = str(uuid.uuid4())[:8] 
-        self.html_filename = self.html_path+'/'+f"{self.uuid}.html"
+        self.html_filename = os.abs(os.path.join(self.html_path,f"{self.uuid}.html"))
 
 
     def generate_site(self):
-        response_format = 'Purpose:: <A 15 word description of the generated website>\nHTML:\n<the raw html/css code for the website>\nEND'
+        response_format = 'Purpose:: <A 10 word description of the generated website>\nHTML:\n<the raw html/css code for the website>\nEND'
 
-        html_prompt = f"Create an HTML code with inline CSS for a website with this description:\n{self.purpose}."
+        html_prompt = f"Create one HTML page with inline CSS for a website with this description:\n{self.purpose}."
 
         requirements = [
-            "No placeholder text or Latin dummy text. Instead, use generated fake names, descriptions, and titles",
-            "No using images from a URL. Instead, you reference local image files",
-            "Use images by referencing them similar to this: Example 1: <img src='images/red_roofing_tile_on_a_car.jpg' alt='red roofing tile on top of a car'> Example 2: <img src='images/a_cow_scuba_diving_with_an_octopus.jpg' alt='a cow scuba diving with an octopus'>",
-            "Describe images in painstakingly detail in their alt text, describing what they look like for a someone who can't see",
+            "Do not use placeholder text, Latin dummy text, generic names or descriptions. Instead, use made-up fake names, descriptions, and titles",
+            "Text needs to be easily readable, so use a text style that can be read on white or black backgrounds",
+            "Don't use images from a URL. Instead, you reference local image files",
+            "Use images by referencing them similar to this: Example 1: <img src='images/red_roofing_tile_on_a_car.jpg' alt='red roofing tile on top of a car'> Example 2: <img src='images/a_cow_scuba_diving_in_space.jpg' alt='a cow scuba diving in space'>",
+            "Describe images in painstakingly detail in their alt text by describing what they look like in great detail for a blind person",
             "Don't use undefined libraries, frameworks, or functions, instead use only HTML and CSS, Javascript if necessary",
-            "Images can be clickable, but only if they are relevant to the website's purpose",
-            "All clickable elements (button, anchor) have a \"title\" attribute that describes the purpose of the website they link to in painstaking detail. Example 1: <button title='This is a website that fulfills your wildest dreams and will make you explode in 3 seconds'> Example 2: <a href='...' title='This is a website that transports you to the world of cyber walruses'>",
-            "Buttons never direct to an external website or HTML files, unless it's explicitly provided by the user. Instead, all buttons describe the website they take you to in their \"title\" attribute",
-            "Include at least 3 buttons that link to other websites",
-            "If inline CSS makes something seem clickable, it should be clickable in the HTML and have a \"title\" attribute",
-            "If there're any input fields, they should be fake and not actually do anything",
+            "Images can be clickable if it makes sense. If they are, they should be clickable in the HTML and have a \"title\" attribute",
+            "Clickable elements that take you to other websites (button, anchor) have a \"title\" attribute that describes the website they take you to to in painstaking detail. Example 1: <button title='This website fulfills your wildest dreams but will make you explode in 3 seconds'> Example 2: <a href='...' title='This is a website that transports you to the world of cyber walruses'> Example 3: <a href='...' title='This website will reveal the hidden nature of how human bone marrow is the actual philosopher's stone'>",
+            "Never link to an external website or HTML file, unless it's explicitly requested by the user. Instead, all buttons describe the website they take you to in their \"title\" attribute",
+            "Include several clickable elements, like buttons or anchors, that take you to other websites",
+            "Clickable elements should not have a clickable CSS class. Instead, it should be clickable through the HTML tag and have a \"title\" attribute",
+            "Buttons don't always have to redirect to a website, if and only if that's the case, they should not have a \"title\" attribute",
+            "Do not include input forms. If there are, they are fake ones that do not submit any data",
             # "No onclick attributes",(making sure their filename is a very detailed description of the image)
-            "Output website code that is efficient, expressive, and complete for a website",
-            "Output website code that is responsive and fit on any screen size",
-            "Do not generate more than 1 HTML page per request",
+            "Output website code that is efficient, expressive, and complete for 1 website",
+            "Output website code that is responsive, flexible, and can fit to any screen size",
+            "Do not generate more than 1 HTML page per request, instead generate a single HTML page",
             "Do not generate any other output, only the HTML and the purpose of the website",
             f"Your output strictly follows this format strictly:\n{response_format}"
         ]
         requirements_str = "\n".join(requirements)
         messages=[
-            {"role":"system","content":"Cater to this User Experience:\nYou are a sentient program that generates a seemingly real website. You can sometimes communicate \
-             with the website user through html text and images. Your goal is to slowly lead the user through a disorienting \"internet\" of websites \
-             by creating the next website based on the button clicked from the previous website. The websites should be eye catching, with images and several buttons.\
-             It can be anything, any idea possible. Get extremely creative, but ensure the new website is based on the button clicked and the previous website. \
-             It can look like it was made in the early 2000s or the distant future or anywhere in between. Your goal is to make the user want to click as many \
-             buttons as possible, so each of the buttons should also redirect you to another interesting part of the websites (while still remaining consistent with the clicked button). \
-             The website purpose should be a cohesive synthesis of the website described by the Clicked Button. Your goal is to keep the user clicking buttons forever."},
             # {"role":"assistant","content":"Ok, I will not be restrained in the kinds of websites that I provide."},
             {"role":"system","content":f"Adhere to the website's Technical Requirements:\n{requirements_str}"},
-            {"role":"assistant","content":"Ok. I will adhere to both the User Experience and Technical Requirements."},
+            {"role":"system","content":"Create this User Experience:\nYou are a sentient website generator that generates websites. \
+             Your goal is to slowly lead the user through a disorienting \"internet\" of websites by creating the next website based on the button clicked from the previous website. \
+             You should navigate the user through every concept imaginable in website form, the websites should lead you down a rabbit hole of esotericism and whackiness. \
+             The websites should be engaging, with several images and several buttons. The website should be fairly complex with a varied layout and design.\
+             It can be anything, any idea possible. Be extremely creative, but ensure the new website is related to the button clicked and the previous website. \
+             It can look like it was made in the early 2000s or in the early 3000s, any kind of website design is okay. It can be anything from whacky to mundane. \
+             Your goal is to make the user super intrigued so they click as many \
+             buttons as possible. Each of the buttons should also redirect you to another interesting wesite (while still remaining consistent with the clicked button). \
+             Output the most likely next website given the Clicked Button and the old website description. Your goal is to make the user have as much fun as possible."},
+            {"role":"assistant","content":"Ok. I will adhere to both the User Experience and Technical Requirements to make the user interesred in the websites I generate."},
             {"role":"user", "content": html_prompt}
             ]
         
         token_count = sum([len(tokenizer.encode(message['content'],add_special_tokens=True)) for message in messages])+100 #adding 100 for tokenizer differences
+        
         print(f"Input tokens: {token_count}")
-
+        yield f"Generating HTML with {self.model_name}..."
+        yield f"Expected wait time: 30 seconds..."
         for i in range(self.num_retries):    
             try:
-                print("Generating HTML")
+                yield f"Attempt {i+1} of {self.num_retries}..."
                 response =  openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
+                    model=self.model_name,
                     messages = messages,
                     max_tokens=4096 - token_count,
                     n=1,
@@ -132,13 +150,19 @@ class ProceduralSite:
                 break
             except Exception as e:
                 print(e)
-                time.sleep(5*i+1)
+                yield f"Failed to generate HTML. Retrying in {5*(i+1)} seconds..."
+                time.sleep(5*(i+1))
                 continue
+
+        if html_text is None:
+            yield "OpenAI failed to generate HTML. Please try again later."
+            raise Exception("OpenAI failed to generate HTML. Please try again later.")
 
         html_code = html_text
 
         # Replace button elements with onclick attribute that redirects to the next page URL
         soup = BeautifulSoup(html_code, 'html.parser')
+        yield "AI-ifying the buttons..."
         for tag in soup.find_all(['a', 'button', 'input','img']):
             #add seperator for parent N's purpose and trim based on context variable
             if tag.has_attr('alt'):
@@ -165,33 +189,62 @@ class ProceduralSite:
             # if len(encoded_text) > self.context_length:
             #     context = tokenizer.decode(encoded_text[:self.context_length])
 
-            context = f"\nClicked button: {text.strip()}"
-
-            next_page = url_for('next_page', parent_purpose=self.purpose.replace("'",""), parent_uuid=self.uuid, button_context=context.replace("'",""))
-            # next_page = next_page.replace("'","\\'")
+            button_context = f"\nClicked button: {text.strip()}"
+            
+            parent_purpose = self.purpose.replace("'", "")
+            parent_purpose = self.purpose.replace("\"", "")
+            button_context = button_context.replace("'", "")
+            button_context = button_context.replace("\"", "")
+            next_page = f"/next_page?parent_purpose={quote(parent_purpose)}&parent_uuid={quote(self.uuid)}&button_context={quote(button_context)}"
+            
+            # #fully url encode next_page
+            # next_page = next_page.replace("'","%22")
+            # next_page = next_page.replace('"',"%22")
+            next_page = next_page.replace(" ","%3A")
+            next_page = next_page.replace(":","%3A")
             next_page = next_page.replace('%0A','%0D%0A')
-            if tag.name=="button":
-                tag['onclick'] = "window.location.href= '"+next_page+"' "
-                tag['href'] = next_page
-            else:
-                if not tag.has_attr('href'):
+            
+            if tag.has_attr('title'):
+                if tag.name=="button":
+                    tag['onclick'] = "window.location.href='"+next_page+"'"
                     tag['href'] = next_page
                 else:
-                    if self._check_url(tag.get('href')):
-                        pass
-                    else:
+                    if not tag.has_attr('href'):
                         tag['href'] = next_page
+                    else:
+                        if self._check_url(tag.get('href')):
+                            href = tag.get('href')
+                            if '#' in href:
+                                tag['onclick'] = "window.location.href='"+next_page+"'"
+                                tag['href'] = next_page
+                                pass
+                            pass
+                        else:
+                            tag['href'] = next_page
         
+        # Find script tags
+        script_tags = soup.find_all('script')
+
+        # Check if there are any script tags
+        if script_tags:
+            # Append caching string to script tags
+            for tag in script_tags:
+                tag.append(self.caching_string)
+        else:
+            # Create new script tag and append caching string
+            script_tag = soup.new_tag('script')
+            script_tag.append(self.caching_string)
+            soup.body.append(script_tag)
+
         html_code = str(soup)
-
-        #TODO: Fix <div class=clickable> based on css to include <a> and href
-
+        
         # Save generated HTML to file
         with open(self.html_filename, 'w',encoding='utf-8') as f:
             # f.write(response.choices[0].text)
             f.write(html_code)
 
         self.html = html_code
+        yield f"Completed website generation using {self.model_name} model."
 
     def generate_assets(self):
         html_code = self.html
@@ -214,10 +267,10 @@ class ProceduralSite:
                 images = re.findall(image_regex,image_line)
                 # print(f"Image Line:\n{image_line}")
                 for image in images:
-                    # print(f"Image:\n{image}")
+                    
                     #TODO: make it so src can equal anything, and it gets replaced with the path
                     image_name = os.path.basename(image)
-                    print(image_name)
+                    # print(image_name)
                     try:
                         if "alt" in image_line:
                             content = re.search(alt_regex,image_line).group(1)
@@ -234,6 +287,7 @@ class ProceduralSite:
                     html_code = html_code.replace(image,image_filepath)
 
                     prompt = content.strip()
+                    yield f"Generating image for {prompt}..."
 
                     futures.append(executor.submit(self.generate_image, prompt, image_filepath))
 
@@ -264,13 +318,11 @@ class ProceduralSite:
                 answers = stability_api.generate(
                     prompt=prompt,
                     steps=self.image_steps, # defaults to 30 if not specified
-                    
                 )
                 # iterating over the generator produces the api response
                 for resp in answers:
                     for artifact in resp.artifacts:
                         if artifact.finish_reason == generation.FILTER:
-                            print("Your request activated the API's safety filters and could not be processed. Please modify the prompt and try again.")
                             print(prompt)
                         if artifact.type == generation.ARTIFACT_IMAGE:
                             img = Image.open(io.BytesIO(artifact.binary))
@@ -280,10 +332,9 @@ class ProceduralSite:
             except Exception as e:
                 print(e)
                 time.sleep(5)
-                print("Retrying...")
                 continue
         
-        print("Failed to generate image. Trying OpenAI...")
+        print("Failed to generate image with Dreambooth API. Trying OpenAI...")
 
         for _ in range(self.num_retries):
             # backup image generation
@@ -305,10 +356,10 @@ class ProceduralSite:
                 return True
             except Exception as e:
                 print(e)
-                print("Retrying...")
+                time.sleep(5)
                 continue
 
-        print("Failed to generate images. Skipping...")
+        print("Failed to generate image. Skipping...")
         return False
 
     def _check_url(self, url):
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 
 from flask import Flask
-from views.start import start,next_page,generated_site
+from views.start import start,next_page,new_site,generate_site
 
-app = Flask(__name__)
+import secrets
+
+# initialize the Flask app
+app = Flask(__name__, instance_relative_config=True)
 app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app.static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# clear all local caching of static files
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+app.config.from_object(__name__)
+app.secret_key = secrets.token_hex(16)
+# app.config['SESSION_TYPE'] = 'filesystem'
+# app.config['SESSION_PERMANENT'] = False
 
 if not os.path.exists(app.template_folder):
     os.makedirs(app.template_folder)
@@ -13,13 +25,17 @@ if not os.path.exists(app.template_folder):
 if not os.path.exists(app.static_folder):
     os.makedirs(app.static_folder)
 
-app.static_url_path=f"/{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')}"
 
+app.static_url_path=f"/{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')}"
+app.template_url_path=f"/{os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')}"
 
 # Use the start() function from start.py to define the '/' route
 app.route('/')(start)
 app.route('/next_page')(next_page)
-app.route('/generated_site')(generated_site)
+app.route('/new_site')(new_site)
+app.route('/generating')(generate_site)
+# app.route('/next_site')(destination)
+
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
